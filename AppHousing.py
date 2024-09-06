4d3a85170da053a709ba6306a5413e24066cb21d
Jesus Muñoz <96089577+Trance-PAW@users.noreply.github.com>
2024-09-06 17:16:31 -0600

    Add files via upload

--- AppHousing.py
+++ AppHousing.py
@@ -5,12 +5,31 @@ Created on Fri Sep  6 17:11:07 2024
 @author: jesus
 """
 
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import streamlit as st 
+from sklearn.base import BaseEstimator, TransformerMixin
 import joblib
+import os
 import urllib.request
 
+
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+        
 # Descargar los archivos del modelo desde GitHub
 @st.cache
 def cargar_modelo():
